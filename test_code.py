da797d8192b08b37292431c4acf445a6eb4c3b35
simporechris <2468242@cegepoutaouais.qc.ca>
2025-11-10 09:45:00 -0500

    chris

--- test_code.py
+++ test_code.py
@@ -1,12 +1,18 @@
 import pytest
 import code as test_code
+from code import calculer_taxe
+
 
 #Fred
 #Arrange
-@pytest.mark.parametrize("produits_choisi","sous_total","prix_total", [
+@pytest.mark.parametrize("produits_choisi","sous_total","prix_total","resultat attentu", [
     (["biscuit", "fromage", "jus d'orange" ], 8.50, 9.77)
 ])
+def test_code1(produits_choisi,sous_total,prix_total):
+
 
 #Act
+resultat = calculer_taxe()
 #Assert
-#assert resultat == resultat_attendu
+resultat_attendu = 9.77
+assert resultat == resultat_attendu
