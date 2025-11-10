684fd8291fbbbd01acef1df546c24672d6a606be
simporechris <2468242@cegepoutaouais.qc.ca>
2025-11-10 10:48:24 -0500

    chris

--- test_code.py
+++ test_code.py
@@ -4,12 +4,11 @@ from solution import calculer_taxe
 
 #Fred
 #Arrange
-@pytest.mark.parametrize("produits_choisi","sous_total","resultat_attendu", [
-    (["biscuit", "fromage", "jus d'orange" ], 8.50, 9.77)
+@pytest.mark.parametrize("produits_choisi,sous_total,resultat_attendu", [
+    (["biscuit", "fromage", "jus d'orange" ], 8.50, (0.43,0.85,9.77))
 ])
 def test_code1(produits_choisi,sous_total,resultat_attendu):
     #Act
     resultat = calculer_taxe(sous_total,produits_choisi)
     #Assert
-    assert isinstance(resultat, float)
     assert resultat == resultat_attendu
