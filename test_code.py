01bda3576827fb1bcc5bad3969f2dd005e351b26
simporechris <2468242@cegepoutaouais.qc.ca>
2025-11-10 09:31:17 -0500

    chris

--- test_code.py
+++ test_code.py
@@ -1,9 +1,11 @@
 import pytest
-
-#import code as code.py
+import code as test_code
 
 #Fred
 #Arrange
+@pytest.mark.parametrize("produits_choisi","sous_total","prix_total", [
+    (["biscuit", "fromage", "jus d'orange" ], 8.50, 9.77)
+])
 
 #Act
 #Assert
