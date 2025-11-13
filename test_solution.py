e72554f935f8833a9412cd4954eda44d124ef8a9
fatoumataawa <6327810@cegepoutaouais.qc.ca>
2025-11-13 15:36:27 -0500

    fatou

---
+++ test_solution.py
@@ -0,0 +1,14 @@
+import pytest
+from solution import calculer_taxe
+
+
+#Fred
+#Arrange
+@pytest.mark.parametrize("produits_choisi,sous_total,resultat_attendu", [
+    (["biscuit", "fromage", "jus d'orange" ], 8.50, (0.43,0.85,9.77))
+])
+def test_solution1(produits_choisi,sous_total,resultat_attendu):
+    #Act
+    resultat = calculer_taxe(sous_total,produits_choisi)
+    #Assert
+    assert resultat == resultat_attendu
