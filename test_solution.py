2e3c001d3d3fe028b16119d2d88c04ecdc2798cd
simporechris <2468242@cegepoutaouais.qc.ca>
2025-11-13 15:40:29 -0500

    chris

--- test_solution.py
+++ test_solution.py
@@ -5,7 +5,7 @@ from solution import calculer_taxe
 #Fred
 #Arrange
 @pytest.mark.parametrize("produits_choisi,sous_total,resultat_attendu", [
-    (["biscuit", "fromage", "jus d'orange" ], 8.50, (0.43,0.85,9.77))
+    (["biscuit", "fromage", "jus d'orange" ], 8.50, (0.43,0.85,9.78))
 ])
 def test_solution1(produits_choisi,sous_total,resultat_attendu):
     #Act
